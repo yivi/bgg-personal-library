5f33cc1212ee34b1321ea7e81d1604f4dd2affd6
Ivan Yivoff <ivan@yivoff.com>
2022-08-10 19:03:52 +0200

    * add expansion handling logic to import process

--- src/Console/BggImporter.php
+++ src/Console/BggImporter.php
@@ -4,10 +4,13 @@
 namespace App\Console;
 
 use App\Bgg\Service;
-use App\Repository\GameRepository;
+use App\Entity\GameCategory;
+use App\Entity\GameDesigner;
+use App\Entity\GameMechanic;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -20,19 +23,54 @@ class BggImporter extends Command
         parent::__construct();
     }
 
+    protected function configure(): void
+    {
+        $this->addArgument('username', InputArgument::REQUIRED, 'BGG Username');
+    }
+
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $gameIds = $this->bggService->getCollection('Portalludico');
+        $this->em->getConnection()->executeQuery('TRUNCATE games');
+        $this->em->getConnection()->executeQuery('TRUNCATE game_mechanics');
+        $this->em->getConnection()->executeQuery('TRUNCATE game_designers');
+        $this->em->getConnection()->executeQuery('TRUNCATE game_categories');
 
-        $games = $this->bggService->getGames(...$gameIds);
+        $gameIds = $this->bggService->getCollection($input->getArgument('username'));
+        $games   = $this->bggService->getGames(...$gameIds);
 
-        dump(count($games));
+        $designers  = [];
+        $mechanics  = [];
+        $categories = [];
 
         foreach ($games as $game) {
+            if ($game->type === 'boardgameexpansion') {
+                if (isset($games[$game->expansionTo])) {
+                    $games[$game->expansionTo]->expansions[] = $game->name;
+                }
+                unset($games[$game->bggId]);
+            }
             $this->em->persist($game);
+
+            $designers  = array_merge($designers, $game->designers);
+            $mechanics  = array_merge($mechanics, $game->mechanics);
+            $categories = array_merge($categories, $game->categories);
         }
 
-        $this->em->getConnection()->executeQuery('TRUNCATE game');
+        foreach (array_unique($designers) as $designer) {
+            $gameDesigner = new GameDesigner($designer);
+            $this->em->persist($gameDesigner);
+        }
+        foreach (array_unique($categories) as $category) {
+            $gameCategory = new GameCategory($category);
+            $this->em->persist($gameCategory);
+        }
+
+        foreach (array_unique($mechanics) as $mechanic) {
+            $gameMechanic = new GameMechanic($mechanic);
+            $this->em->persist($gameMechanic);
+        }
+
         $this->em->flush();
 
         return Command::SUCCESS;
