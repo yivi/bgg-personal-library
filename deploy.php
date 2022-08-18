5940ea221effe19df7a4476e640b7d8009a631de
Ivan Yivoff <ivan@yivoff.com>
2022-08-18 17:12:19 +0200

    dd frontend building to deployment process

--- deploy.php
+++ deploy.php
@@ -12,6 +12,10 @@ add('shared_files', []);
 add('shared_dirs', []);
 add('writable_dirs', []);
 
+task('deploy:frontend', function () {
+    run('cd {{release_or_current_path}} && yarn install --force && yarn encore prod');
+});
+
 // Hosts
 
 host('ludoteca-portal-ludico.yivoff.com')
@@ -21,3 +25,4 @@ host('ludoteca-portal-ludico.yivoff.com')
 // Hooks
 
 after('deploy:failed', 'deploy:unlock');
+after('deploy:vendors', 'deploy:frontend');
