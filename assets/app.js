4cdeacecc9c9cad5ca57371fed3b8b15d6b00d07
Ivan Yivoff <ivan@yivoff.com>
2022-08-18 19:05:49 +0200

    insights-js

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,9 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+import { init, track, parameters } from "insights-js"
+
+init("tW9HZbuLQRgc6Mut")
+
+track.trackPages()
